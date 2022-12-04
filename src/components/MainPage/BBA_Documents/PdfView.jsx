df631f9814bf7a8585569ffbf5322d68d5915e43
Ismayel Hossen <ismayelhossen123@gmail.com>
2022-12-05 00:23:24 +0600

    Add react pdf viwer

--- src/components/MainPage/BBA_Documents/PdfView.jsx
+++ src/components/MainPage/BBA_Documents/PdfView.jsx
@@ -10,11 +10,26 @@ import "../antdstyle.css";
 import { itemRender, onShowSizeChange } from "../paginationfunction";
 import { BaseUrl } from "./CommonUrl";
 import { ColorRing, LineWave } from "react-loader-spinner";
+//react pdf viwer
+import {
+  Icon,
+  MinimalButton,
+  Position,
+  SpecialZoomLevel,
+  Tooltip,
+  Viewer,
+  Worker,
+} from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+// react pdf viwer end
 const PdfView = () => {
   const useParam = useParams();
   let navigate = useNavigate();
   const [DataLoader, setDataLoader] = useState(true);
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
   const PreviousPage = async () => {
     console.log(useParam.recordId);
     navigate(`/ViewDocuments/${useParam.recordId}`);
@@ -54,7 +69,7 @@ const PdfView = () => {
               >
                 <button
                   onClick={() => navigate(-1)}
-                  class="Button_Danger1  float-right mt-4"
+                  class="Button_primary  float-right mt-4"
                   data-dismiss="modal"
                 >
                   <span>Back</span>
@@ -65,48 +80,59 @@ const PdfView = () => {
 
           {/* Page Content */}
 
-          <div class="card-body1">
-            <div className="row">
-              <div className="col-md-12">
-                {DataLoader && (
-                  <>
-                    <div class="row">
-                      <div class="col-md-5"></div>
-                      <div class="col-md-2 mt-4">
-                        <ColorRing
-                          visible={true}
-                          height="80"
-                          width={100}
-                          ariaLabel="blocks-loading"
-                          wrapperStyle={{}}
-                          wrapperClass="blocks-wrapper"
-                          colors={[
-                            "#e15b64",
-                            "#f47e60",
-                            "#f8b26a",
-                            "#abbd81",
-                            "#849b87",
-                          ]}
-                        />
-                      </div>
-                      <div class="col-md-5"></div>
-                    </div>
-                  </>
-                )}
-                {!DataLoader && (
-                  <>
-                    <iframe
+          <div class="card-body1" style={{ padding: "6px 8px" }}>
+            {DataLoader && (
+              <>
+                <div class="row">
+                  <div class="col-md-5"></div>
+                  <div class="col-md-2 mt-4">
+                    <ColorRing
+                      visible={true}
+                      height="80"
+                      width={100}
+                      ariaLabel="blocks-loading"
+                      wrapperStyle={{}}
+                      wrapperClass="blocks-wrapper"
+                      colors={[
+                        "#e15b64",
+                        "#f47e60",
+                        "#f8b26a",
+                        "#abbd81",
+                        "#849b87",
+                      ]}
+                    />
+                  </div>
+                  <div class="col-md-5"></div>
+                </div>
+              </>
+            )}
+            {!DataLoader && (
+              <>
+                {/* <iframe
                       src={`${BaseUrl}/uploadDoc/${useParam.name}`}
                       width="97%"
                       height="500%"
                       style={{ margin: "0 auto" }}
                     >
                       {" "}
-                    </iframe>
-                  </>
-                )}
-              </div>
-            </div>
+                    </iframe> */}
+
+                <div
+                  style={{
+                    height: "750px",
+                    border: "2px solid rgb(244, 244, 244)",
+                  }}
+                >
+                  <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js">
+                    <Viewer
+                      fileUrl={`http://localhost:3000/59.pdf`}
+                      plugins={[defaultLayoutPluginInstance]}
+                      defaultScale={SpecialZoomLevel.PageFit}
+                    />
+                  </Worker>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
